c1a7e90f93ea544ec1109ec2664055648a178575
Aiden Townsend <aidenticklesgood@gmail.com>
2025-09-19 10:15:22 -0500

    Add basic function

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log("JS is working!");
   const states = await (await fetch("states.json")).json();
+
   async function set_state(state) {
-    console.log("code running");
+    console.log("Setting state:", state);
+
+    let root = document.getElementById("root");
+    for (child of root.children) {
+      root.removeChild(child);
+    }
+
     let div = document.createElement("div");
     let state_def = states[state];
-    let header = document.createElement("h1");
+    let header = document.createElement("h2");
     header.innerHTML = state_def.prompt;
     div.appendChild(header);
     console.log(state_def);
 
     for (option of state_def.options) {
-      let paragraph = document.createElement("p");
-      paragraph.innerHTML = option.title;
-      div.appendChild(paragraph);
+      let button = document.createElement("button");
+      button.id = option.next;
+      button.innerHTML = option.title;
+      button.addEventListener("click", async () => {
+        await set_state(button.id);
+      })
+      div.appendChild(button);
     }
 
-    document.getElementById("root").appendChild(div);
+    root.appendChild(div);
   }
+
   set_state("START");
 });
